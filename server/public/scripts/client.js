b1a95fbf61ebe2d28677779c2ff2916d3a187ef1
Nic Bade <chipchapmaniii@gmail.com>
2017-08-27 10:42:30 -0500

    ngRoute set up in client for /rent

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,9 @@
 //console.log('js sourced'); 
 
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/rent', {
+        templateUrl: './views/rent.html'
+    });
+}]); // end app.config
